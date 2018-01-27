b09798692e3d829a995310b8129cf97252b7ea22
Nathnael Alemu <nathnael.alemu@yahoo.com>
2018-01-26 18:43:34 -0600

    started implemented Van Embde Boas Trees

---
+++ VanEmbdeBoasTree.java
@@ -0,0 +1,20 @@
+
+class VanEmbdeBoasCluster{
+	private VanEmbdeBoasCluster[] cluster;
+	private int[] summaryVector;
+	private int universe;//size
+
+
+
+}
+
+public class VanEmbdeBoasTree{
+	private VanEmbdeBoasTree
+
+
+
+}
+
+
+
+
