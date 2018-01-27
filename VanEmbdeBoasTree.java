df2383cb5bbf37bfc1f2713268571d2f286e19c7
Nathnael Alemu <nathnael.alemu@yahoo.com>
2018-01-27 17:33:05 -0600

    implemented some operations of VEB tree

--- VanEmbdeBoasTree.java
+++ VanEmbdeBoasTree.java
@@ -1,78 +1,86 @@
-
-
 class VanEmbdeBoasCluster{
-	private VanEmbdeBoasCluster[] cluster;
-	private int[] summaryVector;
-	private int min;
-	private int max;
+    private VanEmbdeBoasCluster[] cluster;
+    private int[] summaryVector;
+    private int min;
+    private int max;
 
-	public VanEmbdeBoasCluster(int universe){
-		//assert universe is a power of two
-		cluster = new VanEmbdeBoasCluster[Math.sqrt(universe)];
-		summaryVector = new int[cluster.length];
-		for(int i = 0 ; i < cluster.length; i++){
-			cluster[i] = new VanEmbdeBoasCluster(Math.sqrt(universe));
-		}
+    public VanEmbdeBoasCluster(int universe){
+        //assert universe is a power of two
 
-		//Default value for min and max for this cluster
-		min  = -1;
-		max = -1;
+        cluster = new VanEmbdeBoasCluster[(int) Math.sqrt(universe)];
+        summaryVector = new int[cluster.length];
+        if(universe <= 2){
+            for(int i = 0 ; i < cluster.length; i++){
+                cluster[i] = null;
+            }
 
+        }else{
 
-	}
+            for(int i = 0 ; i < cluster.length; i++){
+                cluster[i] = new VanEmbdeBoasCluster((int) Math.sqrt(universe));
+            }
 
-	public VanEmbdeBoasCluster getCluster(int i){
-		return cluster[i];
-	}
-
-	private int high(int x){
-		return x/((int)Math.sqrt(cluster.length));
-	}
-
-	private int low(int x){
-		return x%Math.sqrt(cluster.length);
-	}
-
-	public int getInt(){
-		return min;
-	}
-
-	public int getMax(){
-		return max;
-	}
-	public boolean exists(int value){
-
-		VanEmbdeBoasCluster clusterOfValue = high(value);
-		exists(clusterOfValue,value);
+        }
 
 
-	}
-
-	public int numberOfClusters(){
-		return cluster.length;
-	}
+        //Default value for min and max for this cluster
+        min  = -1;
+        max = -1;
 
 
-	//Wrrapper for the exists interface above
-	private boolean exists(VanEmbdeBoasCluster vebc, int value){
+    }
 
-		if(vebc.numberOfClusters() <= 2){
-			//base case
-			
-			if(vebc.getCluster(0).getMin()< 0 && vebc.getCluster(0).getMax() < 0 && vebc.getCluster(1).getMin()< 0 && vebc.getCluster(1).getMax() < 0){
-				return false;
-			}else{
-				return true;
-			}
+    public VanEmbdeBoasCluster getCluster(int i){
+        return cluster[i];
+    }
 
-		}else{
-		
-			exists(vebc.getCluster(high(value)),low(value));
-		
-		}
+    private int high(int x){
+        return x/((int)Math.sqrt(cluster.length));
+    }
+
+    private int low(int x){
+        return (int)(x%Math.sqrt(cluster.length));
+    }
+
+    public int getMin(){
+        return min;
+    }
+
+    public int getMax(){
+        return max;
+    }
+    public boolean exists(int value){
+
+        VanEmbdeBoasCluster clusterOfValue = getCluster(high(value));
+        return exists(clusterOfValue,value);
+    }
+
+    public int numberOfClusters(){
+        return cluster.length;
+    }
 
 
-	}
+    //Wrrapper for the exists interface above
+    private boolean exists(VanEmbdeBoasCluster vebc, int value){
+
+        if(vebc.numberOfClusters() <= 2){
+            //base case
+
+            if(vebc.getCluster(0).getMin()< 0 && vebc.getCluster(0).getMax() < 0 && vebc.getCluster(1).getMin()< 0 && vebc.getCluster(1).getMax() < 0){
+                return false;
+            }else{
+                return true;
+            }
+
+        }else{
+
+        return exists(vebc.getCluster(high(value)),low(value));
+
+        }
+
+
+
+    }
 
 
 
@@ -80,38 +88,40 @@ class VanEmbdeBoasCluster{
 
 class VanEmbdeBoasTree{
 
-	private VanEmbdeBoasTree vebTree;
-	private int universe;//size
+    private VanEmbdeBoasCluster vebTree;
+    private int universe;//size
 
 
-	public VanEmbdeBoasTree(int size){
-		universe = size;
-		vebTree = new VanEmbdeBoasCluster(universe);//creates an empty VEB Tree
-	}
+    public VanEmbdeBoasTree(int size){
+        universe = size;
+        vebTree = new VanEmbdeBoasCluster(universe);//creates an empty VEB Tree
+    }
 
-	public boolean contains(int value){
-	
-	     return vebTree.exists(value);	
+    public boolean contains(int value){
 
-	}
+        return vebTree.exists(value);
 
-	public void add(int value){
-		//TODO
+    }
 
-	}
+    public void add(int value){
+        //TODO
 
-	public boolean delete(int value){
-		//TODO
+    }
 
-	}
+    public boolean delete(int value){
+        //TODO
+        return false;
+    }
 
-	public int successor(int value){
-		//TODO
-	}
+    public int successor(int value){
+        //TODO
+        return 0;
+    }
 
-	public int predeccessor(int value){
-		//TODO
-	}
+    public int predeccessor(int value){
+        //TODO
+        return 0;
+    }
 
 
 
