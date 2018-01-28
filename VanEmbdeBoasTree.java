4949be5fb961ef60286e39ebe0345b55c59d62c0
Nathnael Alemu <nathnael.alemu@yahoo.com>
2018-01-27 20:14:58 -0600

    implemented add operation of VEB tree

--- VanEmbdeBoasTree.java
+++ VanEmbdeBoasTree.java
@@ -1,27 +1,26 @@
 class VanEmbdeBoasCluster{
     private VanEmbdeBoasCluster[] cluster;
-    private int[] summaryVector;
+    private VanEmbdeBoasCluster summaryVector;
     private int min;
     private int max;
 
     public VanEmbdeBoasCluster(int universe){
         //assert universe is a power of two
 
-        cluster = new VanEmbdeBoasCluster[(int) Math.sqrt(universe)];
-        summaryVector = new int[cluster.length];
+        cluster = new VanEmbdeBoasCluster[universe];
+        summaryVector = null;
         if(universe <= 2){
             for(int i = 0 ; i < cluster.length; i++){
                 cluster[i] = null;
             }
 
-        }else{
+        }else {
 
-            for(int i = 0 ; i < cluster.length; i++){
-                cluster[i] = new VanEmbdeBoasCluster((int) Math.sqrt(universe));
+            for (int i = 0; i < cluster.length; i++) {
+                cluster[i] = null;
             }
 
         }
-
 
         //Default value for min and max for this cluster
         min  = -1;
@@ -51,8 +50,8 @@ class VanEmbdeBoasCluster{
     }
     public boolean exists(int value){
 
-        VanEmbdeBoasCluster clusterOfValue = getCluster(high(value));
-        return exists(clusterOfValue,value);
+        //VanEmbdeBoasCluster clusterOfValue = getCluster(high(value));
+        return exists(this,value);
     }
 
     public int numberOfClusters(){
@@ -62,6 +61,12 @@ class VanEmbdeBoasCluster{
 
     //Wrrapper for the exists interface above
     private boolean exists(VanEmbdeBoasCluster vebc, int value){
+        if(vebc == null){
+            return false;
+        }
+        if(vebc.getMin() == value || vebc.getMax() == value){
+            return true;
+        }
 
         if(vebc.numberOfClusters() <= 2){
             //base case
@@ -81,7 +86,68 @@ class VanEmbdeBoasCluster{
 
 
     }
+    public void setMin(int newMin){
+        this.min = newMin;
+    }
 
+    public void setMax(int newMax){
+        this.max = newMax;
+
+    }
+
+    public void add(int value){
+        add(this,value);
+    }
+    public void setSummaryVectorAt(int i){
+//        assert (summaryVector.length > i && i >= 0);
+//        summaryVector[i] = 1;
+    }
+
+    private void add(VanEmbdeBoasCluster vebc, int value){
+
+        if(vebc.getMin() < 0 ){
+            vebc.setMin(value);
+            if(vebc.getMax() < 0){
+                vebc.setMax(value);
+            }
+        }else{
+            if(value < vebc.getMin()){
+                int intermediate = vebc.getMin();
+                vebc.setMin(value);
+                value = intermediate;
+            }
+
+            if(vebc.numberOfClusters() > 2){
+                if(vebc.getCluster(high(value)) == null){
+                    vebc.setCluster(high(value),new VanEmbdeBoasCluster(high(numberOfClusters())));
+                }
+                if(vebc.summaryVector == null){
+                    vebc.summaryVector = new VanEmbdeBoasCluster(high(numberOfClusters()));
+                }
+                if(vebc.getCluster(high(value)).min == -1){
+                    add(vebc.summaryVector,high(value));
+                    vebc.setMin(low(value));
+                    vebc.setMax(low(value));
+                }
+
+            }
+            if(value > vebc.max){
+                vebc.max = value;
+            }
+
+            add(vebc.getCluster(high(value)),low(value));
+
+
+            // handling summary vectors when recursing back
+
+        }
+
+    }
+
+    private void setCluster(int x, VanEmbdeBoasCluster vanEmbdeBoasCluster) {
+        cluster[x] = vanEmbdeBoasCluster;
+
+    }
 
 
 }
@@ -104,8 +170,7 @@ class VanEmbdeBoasTree{
     }
 
     public void add(int value){
-        //TODO
-
+        vebTree.add(value);
     }
 
     public boolean delete(int value){
@@ -127,13 +192,17 @@ class VanEmbdeBoasTree{
 
 }
 
+
 public class TreeDriver{
 	public static void main(String[] args){
-
-		VanEmbdeBoasTree veb = new VanEmbdeBoasTree(16);
-		boolean fiveIsInSet = veb.contains(5);
-
-		System.out.println(fiveIsInSet);
+ 
+        VanEmbdeBoasTree veb = new VanEmbdeBoasTree(16);
+        int testCase1 = 5;
+        boolean fiveIsInSet = veb.contains(testCase1);
+        System.out.println(fiveIsInSet);
+        veb.add(testCase1);
+        System.out.println("After adding " +testCase1);
+        System.out.println(veb.contains(testCase1));
 	}
 }
 
